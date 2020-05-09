f6b2134dc16873d01b1bb2685f5d88e1aa446be2
Adam Budziak <adambudziak@protonmail.com>
2020-05-09 15:21:50 +0200

    add the core package

--- apollo-crypto-python/src/lib.rs
+++ apollo-crypto-python/src/lib.rs
@@ -3,13 +3,13 @@ use pyo3::wrap_pyfunction;
 
 /// Formats the sum of two numbers as string
 #[pyfunction]
-pub fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
 /// This module is a python module implemented in Rust.
 #[pymodule]
-pub fn string_sum(py: Python, m: &PyModule) -> PyResult<()> {
+fn apollo_crypto(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
 
     Ok(())
